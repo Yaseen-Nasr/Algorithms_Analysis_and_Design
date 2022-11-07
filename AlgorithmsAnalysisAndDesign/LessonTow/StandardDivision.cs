753537952d4f4081da2c7e6bb4383e439303d042
yaseen nasr <yaseennasr1300@gmail.com>
2022-11-07 21:15:44 +0100

    Implementaion for first tow lessons Task

---
+++ AlgorithmsAnalysisAndDesign/LessonTow/StandardDivision.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsAnalysisAndDesign.LessonTow
+{
+    public static class StandardDivision
+    {
+        public static void CalcStandardDivision()
+        {
+
+            double avg = 0, a = 0, b = 0, sb = 0;
+            double[] x;
+            Console.Write("Enter Length of the Set:");
+            int n = int.Parse(Console.ReadLine());
+            x = new double[n];
+
+            Console.WriteLine("Enter all Numbers");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"x[{i}]=");
+                x[i] = int.Parse(Console.ReadLine());
+                avg += x[i];
+            }
+            avg /= n;
+            for (int i = 0; i < n; i++)
+            {
+                a += Math.Pow(x[i] - avg, 2);
+            }
+            b = a / n;
+            sb = Math.Sqrt(b);
+            Console.WriteLine($"Standard Deviation is :  {sb}");
+        }
+    }
+}
---
+++ AlgorithmsAnalysisAndDesign/Program.cs
@@ -0,0 +1,15 @@
+﻿using AlgorithmsAnalysisAndDesign.LessonTow;
+
+namespace AlgorithmsAnalysisAndDesign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(LessoneOne.ParallelogramArea(3,4));
+            //Console.WriteLine(LessoneOne.trapezoidArea(6,12,4));
+            // StandardDivision.CalcStandardDivision();
+            Correlation.CalculateCorrelation();
+        }
+    }
+}
