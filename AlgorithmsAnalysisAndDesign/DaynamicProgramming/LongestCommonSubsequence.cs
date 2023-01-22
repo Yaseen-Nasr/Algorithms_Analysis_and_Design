188cc6e1edc7f498f9b308ca64979f8f4a60684b
yaseen nasr <yaseennasr1300@gmail.com>
2023-01-22 19:33:32 +0100

    Longest Common Subsequence Implementition

---
+++ AlgorithmsAnalysisAndDesign/DaynamicProgramming/LongestCommonSubsequence.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsAnalysisAndDesign.DaynamicProgramming
+{
+	public static class LongestCommonSubsequence
+	{ 
+		public static string Run()
+		{  
+			int i, j, k, t;
+			string s1 = "human";
+			string s2 = "chimpanzee";
+			int s1Len = s1.Length;
+			int s2Len = s2.Length;
+			int[] z = new int[(s1Len + 1) * (s2Len + 1)];
+			int[,] c = new int[(s1Len + 1), (s2Len + 1)];
+			string output="";
+			for (i = 0; i <= s1Len; ++i)
+				c[i, 0] = z[i * (s2Len + 1)];
+
+			for (i = 1; i <= s1Len; ++i)
+			{
+				for (j = 1; j <= s2Len; ++j)
+				{
+					if (s1[i - 1] == s2[j - 1])
+						c[i, j] = c[i - 1, j - 1] + 1;
+					else
+						c[i, j] = Math.Max(c[i - 1, j], c[i, j - 1]);
+				}
+			}
+
+			t = c[s1Len, s2Len];
+			char[] outputSB = new char[t];
+
+			for (i = s1Len, j = s2Len, k = t - 1; k >= 0;)
+			{
+				if (s1[i - 1] == s2[j - 1])
+				{
+					outputSB[k] = s1[i - 1];
+					--i;
+					--j;
+					--k;
+				}
+				else if (c[i, j - 1] > c[i - 1, j])
+					--j;
+				else
+					--i;
+			}
+
+			output = new string(outputSB);
+
+			return output;
+		}
+	}
+}
--- AlgorithmsAnalysisAndDesign/Program.cs
+++ AlgorithmsAnalysisAndDesign/Program.cs
@@ -1,10 +1,5 @@
-﻿using AlgorithmsAnalysisAndDesign.DaynamicPrograming;
-using AlgorithmsAnalysisAndDesign.FractionalKnapsackProblem;
-using AlgorithmsAnalysisAndDesign.GreedyAlgorithm;
-using AlgorithmsAnalysisAndDesign.HuffmanCoding;
-using AlgorithmsAnalysisAndDesign.LessonTow;
-using AlgorithmsAnalysisAndDesign.MegeSort;
-using AlgorithmsAnalysisAndDesign.MergeSort;
+﻿using AlgorithmsAnalysisAndDesign.DaynamicProgramming;
+using System;
 
 namespace AlgorithmsAnalysisAndDesign
 {
@@ -12,9 +7,28 @@ namespace AlgorithmsAnalysisAndDesign
     {
      public static void Main(string[] args)
         {
-            StagecoachProblem.Run();
-
-            Console.ReadKey();
+			Console.WriteLine(LongestCommonSubsequence.Run());
+			//LongestCommonSubsequence.Run();
+			//	string mainStr = "HELLOWORLD";
+			//string subStr = "OHELOD";
+			//int n = mainStr.Length; 
+			//int m = subStr.Length;
+			//int[,] data = new int[m, n];
+			//subStr = " " + subStr;
+			//mainStr = " " + mainStr;
+			//int k = 0;
+			//	for (int i = 0; i < m; i++)
+			//{
+			//	for (int j = 0; j < n; j++)
+			//	{
+			//		if(i.Equals(0) || j.Equals(0) )
+			//		      	data[i,j] = 0;
+			//		else if (subStr[i].Equals(mainStr[j]))
+			//			data[i,j] = k++;
+			//	}
+			//}
+			//Console.WriteLine(data[1, 2]);
+			Console.ReadKey();
         }
       
     }
