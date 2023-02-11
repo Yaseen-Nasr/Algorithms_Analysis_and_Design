51b413dcbde95e8a5da9b5497be9e704ea6e893f
yaseen nasr <yaseennasr1300@gmail.com>
2023-02-11 21:22:44 +0100

    Graph - Breadth First Search

---
+++ AlgorithmsAnalysisAndDesign/Graph/BreadthFirstSearch/Edge.cs
@@ -0,0 +1,14 @@
+﻿namespace AlgorithmsAnalysisAndDesign.Graph.BreadthFirstSearch
+{
+    public class Edge
+    {
+        public Vertex Source { get; }
+        public Vertex Target { get; }
+        public Edge(Vertex source, Vertex target)
+        {
+            this.Source = source;
+            this.Target = target;
+        }
+
+    }
+}
---
+++ AlgorithmsAnalysisAndDesign/Graph/BreadthFirstSearch/Vertex.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsAnalysisAndDesign.Graph.BreadthFirstSearch
+{
+    public class Vertex
+    {
+        public char Point { get; set; }
+        public Edge[] VertexLinxs { get; set; } = null!;
+        public bool IsVisited { get; set; }
+
+    }
+}
